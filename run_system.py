1b515ea56044899601c5b2edd01d57438c02b1d7
sunandag <sunandag@lseg.com>
2017-09-24 20:48:11 +0530

    Parameter cleanup and code refactor

--- run_system.py
+++ run_system.py
@@ -62,6 +62,8 @@ def run_abrupt_drift():
 
     sys_parameters.system_coordinator_initial_dataset_size = 1000
     sys_parameters.system_coordinator_total_sequence_size = 6000
+    sys_parameters.system_coordinator_batch_size = 10
+
     sys_parameters.system_coordinator_drift_scenario = "Abrupt_Drift"
 
     print("System parameters are set: \n{}".format(sys_parameters))
@@ -120,7 +122,12 @@ def run_gradual_drift():
 
     sys_parameters.system_coordinator_initial_dataset_size = 1000
     sys_parameters.system_coordinator_total_sequence_size = 7000
+    sys_parameters.system_coordinator_batch_size = 10
+
     sys_parameters.system_coordinator_drift_scenario = "Gradual_Drift"
+
+    sys_parameters.system_coordinator_drift_period_size = 2000
+    sys_parameters.system_coordinator_mean_dim1_shift = 3
 
     print("System parameters are set: \n{}".format(sys_parameters))
 
@@ -136,9 +143,88 @@ def run_gradual_drift():
     print("Run finished. Exiting system...")
 
 
+
+###################################################################################################
+"""
+Set system parameters and run it with Abrupt_Drift scenario
+"""
+
+def run_recurring_context():
+
+    print("Setting system parameters...")
+    sys_parameters = sys_coord.SystemParameters()
+
+    # Process parameters
+
+    gauss_params = []
+
+    # class 1 Gaussian distribution params
+    mean_1 = [0, 0]
+    cov_1 = [[1, 0], [0, 1]]
+
+    # class 2 Gaussian distribution params
+    mean_2 = [3, 3]
+    cov_2 = [[1, 0], [0, 1]]
+
+    gauss_params.append((mean_1, cov_1))
+    gauss_params.append((mean_2, cov_2))
+
+    sys_parameters.process_num_dimensions = 2
+    sys_parameters.process_num_classes = 2
+    sys_parameters.process_class_distribution_parameters = gauss_params
+
+    # Second set of process paramers (to set after abrupt drift)
+
+    gauss_params = []
+
+    # class 1 Gaussian distribution params
+    mean_1 = [0, 3]
+    cov_1 = [[1, 0], [0, 1]]
+
+    # class 2 Gaussian distribution params
+    mean_2 = [3, 0]
+    cov_2 = [[1, 0], [0, 1]]
+
+    gauss_params.append((mean_1, cov_1))
+    gauss_params.append((mean_2, cov_2))
+
+    sys_parameters.process_class_distribution_parameters_2 = gauss_params
+
+
+    # Other parameters
+
+    sys_parameters.adaptor_submodel_type = "ANN_Submodel"
+
+    sys_parameters.detector_window_size = 500
+
+    sys_parameters.results_manager_avg_error_window_size = 50
+
+    sys_parameters.system_coordinator_initial_dataset_size = 1000
+    sys_parameters.system_coordinator_total_sequence_size = 6000
+    sys_parameters.system_coordinator_batch_size = 10
+
+    sys_parameters.system_coordinator_drift_scenario = "Recurring_Context"
+    sys_parameters.system_coordinator_recurrence_count = 4
+
+    print("System parameters are set: \n{}".format(sys_parameters))
+
+
+    # Create and run SystemCoordinator
+
+    sys_coordinator = sys_coord.SystemCoordinator(sys_parameters)
+
+    print("Starting system... \n{}".format(sys_coordinator))
+
+    sys_coordinator.run()
+
+    print("Run finished. Exiting system...")
+
+
+
 ###################################################################################################
 
 # Call main function
 
 # run_abrupt_drift()
 run_gradual_drift()
+# run_recurring_context()
--- system_coordinator.py
+++ system_coordinator.py
@@ -22,7 +22,7 @@ class SystemParameters:
         self.process_num_dimensions = None
         self.process_num_classes = None
         self.process_class_distribution_parameters = None
-        self.process_class_distribution_parameters_2 = None
+        self.process_class_distribution_parameters_2 = None # For Abrupt_Drift and Recurring_Context drift scenarios
 
         self.detector_window_size = None
 
@@ -33,6 +33,13 @@ class SystemParameters:
         self.system_coordinator_initial_dataset_size = None
         self.system_coordinator_total_sequence_size = None
         self.system_coordinator_drift_scenario = None
+
+        # For "Gradual_Drift" drift scenario
+        self.system_coordinator_drift_period_size = None   # How long the gradual drifting takes
+        self.system_coordinator_mean_dim1_shift = None  # How much the mean of each class distribution will be shifted along dimension 1
+
+        # For "Recurring_Context" drift scenario
+        self.system_coordinator_recurrence_count = None # How many times to switch contexts
 
 
     def __repr__(self):
@@ -92,16 +99,38 @@ class SystemCoordinator:
 
         self.initial_dataset_size = sys_params.system_coordinator_initial_dataset_size
         self.total_sequence_size = sys_params.system_coordinator_total_sequence_size
+        self.batch_size = sys_params.system_coordinator_batch_size  # How many DataPoints are generated per main loop iteration
         self.submodel_type = sys_params.adaptor_submodel_type
         self.drift_scenario = sys_params.system_coordinator_drift_scenario
 
-        # For "Abrupt_Drift" drift scenario
-        self.process_class_distribution_parameters_2 = sys_params.process_class_distribution_parameters_2
-        self.was_drift_occured = False  # To ensure we set the second set of process parameters only once
+        # Parameters specific to scenarios
 
-        # For "Gradual_Drift" drift scenario
-        self.is_left_printed = False  # To print drift start and end only once
-        self.is_right_printed = False
+        if (self.drift_scenario == "Abrupt_Drift"):
+            self.process_class_distribution_parameters_2 = sys_params.process_class_distribution_parameters_2  # Also for Recurring_Context
+            self.was_drift_occured = False  # To ensure we set the second set of process parameters only once
+            self.midpoint = (self.total_sequence_size + self.initial_dataset_size) / 2
+
+        elif (self.drift_scenario == "Gradual_Drift"):
+            self.midpoint = (self.total_sequence_size + self.initial_dataset_size) / 2
+
+            drift_period_size = sys_params.system_coordinator_drift_period_size
+            self.drift_start_seq = self.midpoint - drift_period_size / 2
+            self.drift_end_seq = self.midpoint + drift_period_size / 2
+
+            self.increment = sys_params.system_coordinator_mean_dim1_shift * self.batch_size / drift_period_size
+
+            self.is_left_printed = False  # To print drift start and end only once
+            self.is_right_printed = False
+
+        elif (self.drift_scenario == "Recurring_Context"):
+            self.recurrence_count = sys_params.system_coordinator_recurrence_count
+
+            self.between_switch_size = (self.total_sequence_size + self.initial_dataset_size) / self.recurrence_count
+            assert self.between_switch_size >= 3 * self.detector.window_size  # To have enough diff-stable periods between switches
+            self.next_switch_point = self.between_switch_size
+
+        else:
+            assert False
 
 
     def __repr__(self):
@@ -112,41 +141,38 @@ class SystemCoordinator:
     # Given the current seq no, set time varying process parameters
     def set_process_parameters(self, seq):
         if (self.drift_scenario == "Abrupt_Drift"):
-            midpoint = (self.total_sequence_size + self.initial_dataset_size)/2
-            if (self.was_drift_occured == False and seq >= midpoint):
+
+            if (self.was_drift_occured == False and seq >= self.midpoint):
                 print("Abrupt_Drift scenario: changing process parameters to second set")
                 self.process.set_class_distribution_params(self.process_class_distribution_parameters_2)
                 print(self.process)
                 self.was_drift_occured = True
 
         elif (self.drift_scenario == "Gradual_Drift"):
-            midpoint = (self.total_sequence_size + self.initial_dataset_size)/2
 
-            drift_period_size = 2000
-            left = midpoint - drift_period_size/2
-            right = midpoint + drift_period_size/2
+            if (seq >= self.drift_start_seq and seq <= self.drift_end_seq):
+                for class_distribution_params in self.process.class_distribution_params:
+                    mean = class_distribution_params[0]
+                    mean[0] += self.increment   # Increment mean along dimension 1
 
-            batch_size = 10 # MUST BE PARAMETERIZED (same as used in run())
-            increment = 3*batch_size/drift_period_size
-            if (seq >= left and seq <= right):
-                mean_1 = self.process.class_distribution_params[0][0]
-                mean_2 = self.process.class_distribution_params[1][0]
-                mean_1[0] += increment
-                mean_2[0] += increment
                 self.process.set_class_distribution_params(self.process.class_distribution_params)
 
-            if (seq >= left and self.is_left_printed == False):
+            if (seq >= self.drift_start_seq and self.is_left_printed == False):
                 self.results_manager.add_special_marker(seq, "drift_start")
                 self.is_left_printed = True
                 print("Gradual drift started: seq={}, process={}".format(seq, self.process))
 
-            if (seq >= right and self.is_right_printed == False):
+            if (seq >= self.drift_end_seq and self.is_right_printed == False):
                 self.results_manager.add_special_marker(seq, "drift_end")
                 self.is_right_printed = True
                 print("Gradual drift finished: seq={}, process={}".format(seq, self.process))
 
         elif (self.drift_scenario == "Recurring_Context"):
-            assert False
+
+            # if (seq >= self.next_switch_point):
+            #
+            #     self.next_switch_point += self.between_switch_size
+
         else:
             assert False
 
@@ -183,11 +209,9 @@ class SystemCoordinator:
         self.train_initial_model()
 
         # parameterize
-        batch_size = 10
         info_print_interval = 2000
         progress_print_interval = 100
         detection_batch_size = 10
-
 
         total_samples = 0
         res_manager_seq_num = self.initial_dataset_size - 1 # Because a result set was added in train_initial_model
@@ -197,13 +221,13 @@ class SystemCoordinator:
         detection_counter = 0
 
         while(True):
-            batch = self.process.generate_data_points_from_all_labels(total_count=batch_size)
-            total_samples += batch_size
-            res_manager_seq_num += batch_size
+            batch = self.process.generate_data_points_from_all_labels(total_count=self.batch_size)
+            total_samples += self.batch_size
+            res_manager_seq_num += self.batch_size
 
-            info_print_counter += batch_size
-            progress_print_counter += batch_size
-            detection_counter += batch_size
+            info_print_counter += self.batch_size
+            progress_print_counter += self.batch_size
+            detection_counter += self.batch_size
 
             if (total_samples > self.total_sequence_size):
                 break
